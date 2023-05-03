96586cc2210fb66074bbac762799b306d71153be
Luis Miguel Jimenez Muñoz <jimenezmu@esat-alumni.com>
2023-05-03 22:03:19 +0100

    Mejoras en el index, cambio de icono, politica de privacidad y primer concepto de formulario y personajes

--- proyecto.js
+++ proyecto.js
@@ -6,34 +6,63 @@
 // });
 
 
-function toggleMenu() {
-    var menu = document.getElementById("menu");
-    menu.classList.toggle("show");
-}
+// function toggleMenu() {
+//     var menu = document.getElementById("menu");
+//     menu.classList.toggle("show");
+// }
 
+
+// Funcion para la aparicion y desparicion de la barra superior 
+// al subir y bajar con el ratón
 var prevScrollpos = window.pageYOffset;
 var navbar = document.querySelector('.navbar');
 window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos)
-    {
-        navbar.classList.remove('hidden');
-    } else
-    {
-        navbar.classList.add('hidden');
+    if (window.scrollY > 1200) {
+        if (prevScrollpos > currentScrollPos) {
+            navbar.classList.remove('hidden');
+        } else {
+            navbar.classList.add('hidden');
+        }
     }
     prevScrollpos = currentScrollPos;
 }
+///////////////////////////////////////////////////////////////
 
-window.addEventListener("scroll", function() {
-    var barra = document.getElementById('navbar');
-    var boton = document.getElementById("button");
+// window.addEventListener("scroll", function() {
+//     var barra = document.getElementById('navbar');
+//     var boton = document.getElementById("button");
 
-    if (barra.offsetTop + barra.offsetHeight <= window.pageYOffset) {
-        boton.style.display = "block";
-    } else
+//     if (barra.offsetTop + barra.offsetHeight <= window.pageYOffset) {
+//         boton.style.display = "block";
+//     } else
+//     {
+//         boton.style.display = "none";
+//     }
+
+// });
+
+// const character1 = document.getElementById("character1");
+// const info = document.getElementById("info");
+
+// character1.addEventListener("click", () => {
+//     console.log("click activado");
+//     character1.style.transform = "translateY(-50px)";
+//     info.innerHTML = "Informacion del item 1";
+//     info.style.display = "block";
+// });
+
+function showMessage() {
+    console.log("Mensaje de consola al hacer click en el div.");
+}
+ 
+function DisplaySideInfo(){
+    var x = document.getElementById("info");
+    var y = document.getElementById("character-item")
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    }else
     {
-        boton.style.display = "none";
+        x.style.display = "none";
     }
-
-});
+}
